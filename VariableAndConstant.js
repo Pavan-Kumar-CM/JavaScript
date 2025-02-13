09b34132d87cd6c4356f24b9924d016f41f77b8e
PAVAN30627 <vvce21cse0055@vvce.ac.in>
2025-02-13 18:10:06 +0530

    Variable and Constant Complete

---
+++ VariableAndConstant.js
@@ -0,0 +1,19 @@
+//  Constant
+const USN = "4VV21CS108"
+// USN = "PavanVV21CS108" NotAllowed Due to const
+
+
+let userName = "Pavan Kumar C M"
+userName = "Pavan Gowda" // allowed
+
+let password = "Pav@123"
+
+var gender = "Male"; //var have issue in block and functional scope Prefer to Use (let) Always
+
+let marks; // Undefined Variable  
+
+console.log("USN: " + USN)
+console.log("Name: " + userName)
+console.log("Gender: " + gender)
+
+console.table([USN, userName, gender, marks]) // Show All The Values in table formate -> (index) | values
